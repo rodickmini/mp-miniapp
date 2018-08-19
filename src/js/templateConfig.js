726c1fdad46f80d687d378efb59a5e35ec7b6d58
caiyou <rodickcai@gmail.com>
2018-08-19 15:48:32 +0800

    UPDATE 选择名片

---
+++ src/js/templateConfig.js
@@ -0,0 +1,51 @@
+module.exports = {
+  version: '1.0',
+  tplList: {
+    0: {
+      catName: '商务',
+      list: [
+        {
+          id: 0,
+          name: '高级灰',
+          thumb: 'http://dfdfsa.com/aaa.jpg',
+          cardBg: 'http://dfdfsa.com/bg.jpg',
+          darkColor: '#fff',
+          lightColor: 'rgba(255,255,255,.7)',
+          btnColor: 'blue'
+        },
+        {
+          id: 1,
+          name: '高级灰',
+          thumb: 'http://dfdfsa.com/aaa.jpg',
+          cardBg: 'http://dfdfsa.com/bg.jpg',
+          darkColor: '#fff',
+          lightColor: 'rgba(255,255,255,.7)',
+          btnColor: 'blue'
+        }
+      ]
+    },
+    1: {
+      catName: '可爱',
+      list: [
+        {
+          id: 2,
+          name: '高级灰',
+          thumb: 'http://dfdfsa.com/aaa.jpg',
+          cardBg: 'http://dfdfsa.com/bg.jpg',
+          darkColor: '#fff',
+          lightColor: 'rgba(255,255,255,.7)',
+          btnColor: 'blue'
+        },
+        {
+          id: 3,
+          name: '高级灰',
+          thumb: 'http://dfdfsa.com/aaa.jpg',
+          cardBg: 'http://dfdfsa.com/bg.jpg',
+          darkColor: '#fff',
+          lightColor: 'rgba(255,255,255,.7)',
+          btnColor: 'blue'
+        }
+      ]
+    },
+  }
+}
