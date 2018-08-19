c1c29862a62e609e7f3b4c9476e1b8efda009283
caiyou <rodickcai@gmail.com>
2018-08-19 16:43:11 +0800

    UPDATE 名片模板

--- src/js/templateConfig.js
+++ src/js/templateConfig.js
@@ -1,51 +1,71 @@
 module.exports = {
   version: '1.0',
-  tplList: {
-    0: {
+  tplList: [
+    {
+      id: 0,
+      catId: 0,
       catName: '商务',
-      list: [
-        {
-          id: 0,
-          name: '高级灰',
-          thumb: 'http://dfdfsa.com/aaa.jpg',
-          cardBg: 'http://dfdfsa.com/bg.jpg',
-          darkColor: '#fff',
-          lightColor: 'rgba(255,255,255,.7)',
-          btnColor: 'blue'
-        },
-        {
-          id: 1,
-          name: '高级灰',
-          thumb: 'http://dfdfsa.com/aaa.jpg',
-          cardBg: 'http://dfdfsa.com/bg.jpg',
-          darkColor: '#fff',
-          lightColor: 'rgba(255,255,255,.7)',
-          btnColor: 'blue'
-        }
-      ]
+      name: '黑金',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/heijin/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/heijin/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
     },
-    1: {
-      catName: '可爱',
-      list: [
-        {
-          id: 2,
-          name: '高级灰',
-          thumb: 'http://dfdfsa.com/aaa.jpg',
-          cardBg: 'http://dfdfsa.com/bg.jpg',
-          darkColor: '#fff',
-          lightColor: 'rgba(255,255,255,.7)',
-          btnColor: 'blue'
-        },
-        {
-          id: 3,
-          name: '高级灰',
-          thumb: 'http://dfdfsa.com/aaa.jpg',
-          cardBg: 'http://dfdfsa.com/bg.jpg',
-          darkColor: '#fff',
-          lightColor: 'rgba(255,255,255,.7)',
-          btnColor: 'blue'
-        }
-      ]
+    {
+      id: 1,
+      catId: 0,
+      catName: '商务',
+      name: '活力',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/huoli/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/huoli/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
     },
-  }
+    {
+      id: 2,
+      catId: 0,
+      catName: '商务',
+      name: '可爱兔',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/keaitu/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/keaitu/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
+    },
+    {
+      id: 3,
+      catId: 0,
+      catName: '商务',
+      name: '镭射',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/leishe/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/leishe/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
+    },
+    {
+      id: 4,
+      catId: 0,
+      catName: '商务',
+      name: '墨竹',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/mozhu/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/mozhu/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
+    },
+    {
+      id: 5,
+      catId: 0,
+      catName: '商务',
+      name: '土豪金',
+      thumb: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/tuhaojin/thumb.png',
+      cardBg: 'http://pcxxelj6y.bkt.clouddn.com/theme/v1/img/tuhaojin/bg.png',
+      darkColor: '#fff',
+      lightColor: 'rgba(255,255,255,.7)',
+      btnColor: 'blue'
+    }
+  ]
 }
